23962adffca1fdc66e2c294416db3091bcfbf6a0
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-10-10 09:14:23 -0300

    a

--- src/Rotas/Rotas.js
+++ src/Rotas/Rotas.js
@@ -2,18 +2,18 @@ import React from 'react'
 import { BrowserRouter, Routes, Route,} from 'react-router-dom'
 import Home from '../Components/Home'
 import Projetos from '../Components/Projetos'
-import Sobre from '../Components/Sobre'
+import About from '../Components/About'
 import Header from './Header'
 
 export default function Rotas() {
   return (
     <BrowserRouter>
-    <Header Rota1='Home'  Rota3='Projetos' Rota4='Sobre'/>
+    <Header Rota1='Home'  Rota3='Projetos' Rota4='About'/>
 
     <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/Projetos" element={<Projetos/>}/>
-    <Route path='/Sobre' element={<Sobre/>}/>
+    <Route path='/About' element={<About/>}/>
     </Routes>
     </BrowserRouter>
     
