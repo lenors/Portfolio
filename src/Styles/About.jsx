1542a98cc58aa029fa3650d68e5ec9fd1f80b4c4
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-10-11 14:28:18 -0300

    Corrigido

--- src/Styles/About.jsx
+++ src/Styles/About.jsx
@@ -1,78 +1,75 @@
 import styled from "styled-components";
 export const Section2 = styled.div`
-display:flex;
-align-items:center;
-justify-content:center;
-flex-direction:column;
-margin-top:2rem;
-
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 2rem;
+`;
 export const Image = styled.img`
-width: 300px;
-height:300px;
-border-radius: 10px 10px 10px 10px;
-border:solid black 2px;
-`
+  width: 300px;
+  height: 300px;
+  border-radius: 10px 10px 10px 10px;
+  border: solid black 2px;
+`;
 
 export const Who = styled.h1`
-    font-family: 'Press Start 2P', cursive;
-    color: black;
-    animation: typing 1s 1s normal steps(10) both, blink 500ms  infinite alternate ;
-    white-space: nowrap;
-    overflow: hidden;
-    border-right: 3px solid;
-    margin-top:1rem;
-    @keyframes typing {
-        from{
-            width: 0;
-        }
-        to{
-            width: 9em;
-        }
+  font-family: "Press Start 2P", cursive;
+  color: black;
+  animation: typing 1s 1s normal steps(10) both, blink 500ms infinite alternate;
+  white-space: nowrap;
+  overflow: hidden;
+  border-right: 3px solid;
+  margin-top: 1rem;
+  @keyframes typing {
+    from {
+      width: 0;
     }
-    @keyframes blink {
-        from{
-            border-right: 3px solid;
-        }
-        to{
-            border-color: transparent;
-        }
+    to {
+      width: 9em;
     }
-`
+  }
+  @keyframes blink {
+    from {
+      border-right: 3px solid;
+    }
+    to {
+      border-color: transparent;
+    }
+  }
+`;
 
 export const Paragrafo = styled.p`
-font-family: 'Montserrat', sans-serif;
-width:500px;
-font-size:2rem;
-font-weight:700;
-@media (max-width: 768px){
-    width:300px;
-    font-size:30px;
-    color:white;
-}
-`
+  font-family: "Montserrat", sans-serif;
+  width: 500px;
+  font-size: 2rem;
+  font-weight: 700;
+  @media (max-width: 768px) {
+    width: 300px;
+    font-size: 30px;
+    color: white;
+  }
+`;
 export const Language = styled.div`
-display:flex;
-flex-direction:row;
-align-items:center;
-justify-content:center;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
 export const Icons3 = styled.ul`
-
-width:100px;
-display:flex;
-list-style:none;
-align-items:center;
-justify-content:center;
-@media (max-width: 768px){
-    
-}
-`
+  width: 100px;
+  display: flex;
+  list-style: none;
+  align-items: center;
+  justify-content: center;
+  @media (max-width: 768px) {
+  }
+`;
 export const Icons4 = styled.li`
-color:white;
-width:50px;
-font-size:50px;
-@media (max-width: 768px){
-    color:white;
-}
-`
+  color: white;
+  width: 50px;
+  font-size: 50px;
+  @media (max-width: 768px) {
+    color: white;
+  }
+`;
