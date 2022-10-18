ac3df5cbe5fccc2cba0b751f0f3638583ab9cad7
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-10-18 09:57:58 -0300

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Globalstyle } from '../Styles/Globalstyle'
+import Pokedex from '../Imagens/Pokedex.png'
 
 export default function Projetos() {
   return (
     <div>
       <Globalstyle/>
-     <figure></figure>
-     <figure></figure>
-     <figure></figure>
+     <figure>
+      <img src={Pokedex} alt="" />
+      <h2>Pokedex</h2>
+     </figure>
+     <figure>
+      <img src="" alt="" />
+     </figure>
+     <figure>
+      <img src="" alt="" />
+     </figure>
      <figure></figure>
      <figure></figure>
      <figure></figure>
